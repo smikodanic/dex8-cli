403ad41a71c5b700700ec7c90acf74aa65b85a7f
Sasa M <smikodanic@gmail.com>
2023-08-06 15:31:52 +0200

    t1

--- bin/task_templates/t1/main.js
+++ bin/task_templates/t1/main.js
@@ -1,11 +1,16 @@
 const FunctionFlow = require('functionflowx');
 const Echo = require('echoer');
+const { EventEmitter } = require('events');
 
 const f1 = require('./f1.js');
 
 
-module.exports = async (input, eventEmitter) => {
+module.exports = async (input) => {
   if (!input) { throw new Error('Input is required.'); }
+
+  // create event emitter
+  const eventEmitter = new EventEmitter();
+  eventEmitter.setMaxListeners(5); // 10 by default
 
   const ff = new FunctionFlow({ debug: false, msDelay: 800 }, eventEmitter);
   const echo = new Echo(true, 10, eventEmitter);
